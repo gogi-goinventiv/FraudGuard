4697f5531a3b2727c17f8f1ce9b76fc1563cd231
gogi-goinventiv <gogi.goinventiv@gmail.com>
2025-06-04 15:19:24 +0530

    Create install.js

---
+++ pages/api/auth/install.js
@@ -0,0 +1,23 @@
+// pages/api/auth/install.js
+import sessionHandler from '../utils/sessionHandler';
+
+export default async function handler(req, res) {
+    const { shop } = req.query;
+
+  if (req.method === 'GET') {
+    try {
+      const session = await sessionHandler.loadSession(shop);
+
+      if (session) {
+          res.redirect(302, `/?shop=${shop}`);
+      }
+
+      res.redirect(302, `/api/auth`);
+    } catch (error) {
+      console.error('Auth begin error:', error);
+      res.status(500).send('Internal Server Error');
+    }
+    return;
+  }
+  res.status(405).send('Method Not Allowed');
+}
