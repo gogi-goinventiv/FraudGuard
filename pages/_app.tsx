62129bcc5250e728ea82d0201913f2f911165838
gogi-goinventiv <gogi.goinventiv@gmail.com>
2025-06-09 23:55:47 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 import React, { useState } from 'react';
 import { ManualCaptureWarningContext } from '../context/manualCaptureWarning';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [manualCaptureWarning, setManualCaptureWarning] = useState(false);
-
+  
   return (
     <React.Suspense>
+      <Head>
+        <meta name="shopify-api-key" content={process.env.NEXT_PUBLIC_SHOPIFY_API_KEY} />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+      </Head>
       <ManualCaptureWarningContext.Provider value={{ manualCaptureWarning, setManualCaptureWarning }}>
         <Component {...pageProps} />
       </ManualCaptureWarningContext.Provider>
