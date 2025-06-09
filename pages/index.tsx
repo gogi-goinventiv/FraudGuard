9a63bc2d15f80a38dbcdda04b182ed4364055309
gogi-goinventiv <gogi.goinventiv@gmail.com>
2025-06-10 00:34:45 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,18 +14,18 @@ export default function Home() {
 
   const MIN_LOADING_TIME = 1000;
 
+  const app = createApp({
+        apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY!,
+        host: host as string || 'YWRtaW4uc2hvcGlmeS5jb20vc3RvcmUvdXZzemgxLW01',
+        forceRedirect: true,
+      });
+
   useEffect(() => {
     if (!shop) return;
 
     // Ensure we are running inside the Shopify Admin iframe
     if (window.top === window.self) {
       // Not embedded – redirect to embedded version
-      const app = createApp({
-        apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY!,
-        host: host as string || 'YWRtaW4uc2hvcGlmeS5jb20vc3RvcmUvdXZzemgxLW01',
-        forceRedirect: true,
-      });
-
       const redirect = Redirect.create(app);
       redirect.dispatch(Redirect.Action.ADMIN_PATH, `/apps/${process.env.NEXT_PUBLIC_APP_NAME || 'your-app'}`); 
       return;
