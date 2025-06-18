a663650053624a2143334d1f2cdcfd84513946a2
gogi-goinventiv <gogi.goinventiv@gmail.com>
2025-06-18 06:52:54 +0530

    install session adjustment fix

--- pages/api/auth/install.js
+++ pages/api/auth/install.js
@@ -1,9 +1,18 @@
 // pages/api/auth/install.js
+import sessionHandler from '../utils/sessionHandler';
+
 export default async function handler(req, res) {
     const { shop, host } = req.query;
 
     if (req.method === 'GET') {
+
         try {
+            const session = await sessionHandler.loadSession(shop);
+
+            if (session) {
+                res.redirect(302, `/?shop=${shop}&host=${host}`);
+                return;
+            }
             res.redirect(302, `/api/auth?shop=${shop}&host=${host}`);
         } catch (error) {
             console.error('Auth begin error:', error);
